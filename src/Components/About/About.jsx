dd9bf4c01345f5e79b433d6767ef51af3ac5a22c
selam24 <selamtessera@gmail.com>
2024-10-20 19:18:39 -0400

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./about.module.css";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
