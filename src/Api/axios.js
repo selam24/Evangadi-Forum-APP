a95d0a337918451c80eaade4d1090693ac552ca3
selam24 <selamtessera@gmail.com>
2024-10-30 20:45:44 -0400

    commit6

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL: "https://selam-backend-evangadi.onrender.com/api",
-  // baseURL: "https://backend-evangadi-wlxy.onrender.com/api",
-  baseURL: "https://evangadi-forum-backend-mcyd.onrender.com/api",
+  baseURL: "https://backend-evangadi-wlxy.onrender.com/api",
 });
 export default instance;
